d3d8112d5b7696cedc341418cad7911f7362ddc1
maalex98 <47811847+maalex98@users.noreply.github.com>
2020-04-22 18:29:22 +0300

    index

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+  include './views/header.php';
+?>
+
+    <section>
+      <h2 class="index_h2">
+        Welcome to our Fashion e-Shop!
+      </h2>
+      <div class="galery">
+	        <div class="column">
+	          <p>Men</p>
+	          <a href="men.php">
+	          	<img class="image" src="imagini/men.jpg">
+	      	  </a>
+	        </div>
+
+	        <div class="column">
+	        	<p>
+	           		Women
+	        	</p>
+	          	<a href="women.php">
+	          		<img class="image" src="imagini/women.jpg">
+	        	</a>
+	        </div>
+
+	        <div class="column">
+	            <p>
+	          		Kids
+	      	    </p>
+	         	<a href="kids.php">
+	            	<img class="image" src="imagini/kids.jpg">
+	        	</a>
+	        </div>
+      </div>
+    </section>
+
+    <br />
+    <br />
+    <br />
+    <footer>
+	      <div class="share">
+	          	<div class="logos">
+
+	          		<p>FOLLOW US</p>
+		            <a class="index_logo" href="https://www.facebook.com">
+		              <i class="fa fa-facebook-square"></i>
+		          	</a>
+
+		            <a class="index_logo" href="https://www.twitter.com">
+		              <i class="fa fa-twitter-square"></i>
+		          	</a>
+
+					<a class="index_logo" href="https://www.instagram.com">
+		              <i class="fa fa-youtube-square"></i>
+		          	</a>
+
+		            <a class="index_logo" href="https://www.instagram.com">
+		              <i class="fa fa-instagram"></i>
+		          	</a>
+
+	        	</div>
+	      	</div>
+	    </div>
+    </footer>
+  </body>
+</html>

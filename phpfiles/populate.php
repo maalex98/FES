cc3f7c19cc8e315157426f26611d7354e2a19fd8
maalex98 <47811847+maalex98@users.noreply.github.com>
2020-06-09 17:16:45 +0300

    Kids products

--- phpfiles/populate.php
+++ phpfiles/populate.php
@@ -13,7 +13,7 @@ $seasons = array('summer', 'winter', 'autumn', 'spring');
 $fabrics = array('leather', 'cotton', 'unknown');
 $descriptions = array('Nice Shoes', 'Nice Jacket', 'Nice Shirt');
 
-$NumberOfEach = 3;
+$NumberOfEach = 12;
 
 // women shoes
 for($it=0; $it<$NumberOfEach ; $it=$it+1){
@@ -139,6 +139,70 @@ for($it=0; $it<$NumberOfEach ; $it=$it+1){
 	$sql = "INSERT INTO products (type, name, price, image_path, gender, event, season, style, brand, color, fabric,description, stock) VALUES ('" . $types[2] . "','" . $name . "', '" . $price . "','" . $image_path . "','" . $gender . "','" . $events[$eventNumber] . "' ,'" . $seasons[$seasonNumber] . "','" . $styles[$styleNumber] . "','" . $brands[$brandNumber] . "','" . $colors[$colorNumber] ."','" . $fabrics[$fabricNumber]."','" . $descriptions[2] . "','" . $stock . "')";
 	$result = mysqli_query($conn, $sql);
 }
+
+// kids shoes
+for($it=0; $it<$NumberOfEach ; $it=$it+1){
+	$colorNumber = rand(0,2);
+	$sizeNumber = rand(0,7);
+	$name = $colors[$colorNumber].' shoes ' . $shoesSizes[$sizeNumber];
+	$price = rand(100,500);
+	$rand_number=rand(1,4);
+	$gender= 'kids';
+	$eventNumber = rand(0,3);
+	$seasonNumber = rand(0,3);
+	$styleNumber = rand(0,2);
+	$brandNumber = rand(0,4);
+	$fabricNumber = rand(0,2);
+	$stock=rand(1,100);
+
+	$image_path = 'images/kids_products/' . $colors[$colorNumber] . $types[0] . $rand_number.'.jpg';
+	$sql = "INSERT INTO products (type, name, price, image_path, gender, event, season, style, brand, color, fabric,description, stock) VALUES ('" . $types[0] . "','" . $name . "', '" . $price . "','" . $image_path . "','" . $gender . "','" . $events[$eventNumber] . "' ,'" . $seasons[$seasonNumber] . "','" . $styles[$styleNumber] . "','" . $brands[$brandNumber] . "','" . $colors[$colorNumber] ."','" . $fabrics[$fabricNumber]."','" . $descriptions[0] . "','" . $stock . "')";
+	$result = mysqli_query($conn, $sql);
+}
+
+//kids jackets
+
+for($it=0; $it<$NumberOfEach ; $it=$it+1){
+	$colorNumber = rand(0,2);
+	$sizeNumber = rand(0,3);
+	$name = $colors[$colorNumber].' jacket ' . $jacketsSizes[$sizeNumber];
+	$price = rand(100,500);
+	$rand_number=rand(1,4);
+	$gender= 'kids';
+	$eventNumber = rand(0,3);
+	$seasonNumber = rand(0,3);
+	$styleNumber = rand(0,2);
+	$brandNumber = rand(0,4);
+	$fabricNumber = rand(0,2);
+	$stock=rand(1,100);
+
+	$image_path = 'images/kids_products/' . $colors[$colorNumber] . $types[1] . $rand_number.'.jpg';
+	$sql = "INSERT INTO products (type, name, price, image_path, gender, event, season, style, brand, color, fabric,description, stock) VALUES ('" . $types[1] . "','" . $name . "', '" . $price . "','" . $image_path . "','" . $gender . "','" . $events[$eventNumber] . "' ,'" . $seasons[$seasonNumber] . "','" . $styles[$styleNumber] . "','" . $brands[$brandNumber] . "','" . $colors[$colorNumber] ."','" . $fabrics[$fabricNumber]."','" . $descriptions[1] . "','" . $stock . "')";
+	$result = mysqli_query($conn, $sql);
+}
+
+
+//kids shirts
+
+for($it=0; $it<$NumberOfEach ; $it=$it+1){
+	$colorNumber = rand(0,2);
+	$sizeNumber = rand(0,3);
+	$name = $colors[$colorNumber].' shirt ' . $jacketsSizes[$sizeNumber];
+	$price = rand(100,500);
+	$rand_number=rand(1,4);
+	$gender= 'kids';
+	$eventNumber = rand(0,3);
+	$seasonNumber = rand(0,3);
+	$styleNumber = rand(0,2);
+	$brandNumber = rand(0,4);
+	$fabricNumber = rand(0,2);
+	$stock=rand(1,100);
+
+	$image_path = 'images/kids_products/' . $colors[$colorNumber] . $types[2] . $rand_number.'.jpg';
+	$sql = "INSERT INTO products (type, name, price, image_path, gender, event, season, style, brand, color, fabric,description, stock) VALUES ('" . $types[2] . "','" . $name . "', '" . $price . "','" . $image_path . "','" . $gender . "','" . $events[$eventNumber] . "' ,'" . $seasons[$seasonNumber] . "','" . $styles[$styleNumber] . "','" . $brands[$brandNumber] . "','" . $colors[$colorNumber] ."','" . $fabrics[$fabricNumber]."','" . $descriptions[2] . "','" . $stock . "')";
+	$result = mysqli_query($conn, $sql);
+}
+
 
 
 
